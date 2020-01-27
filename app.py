cfc4f944683fca10869b5a17b2b9ef1a05b1ca46
Tran Cuong Truc <cuong.t@scopicsoftware.com>
2020-01-27 23:50:47 +0700

    Add logger configuration file

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import logging
+import logging.config
+
+import yaml
 
 from schedulers.domain_update_scheduler import DomainUpdateScheduler
 from schedulers.schedule_task_runner import ScheduledTasksRunner
 
 
+def config_logger():
+    with open('resources/log.yml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+
+
 def main():
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Starting cloudflare-ddns application...")
+    config_logger()
+    logging.info("Started cloudflare-ddns application...")
     domain_update_scheduled_task = DomainUpdateScheduler()
     scheduler_runner = ScheduledTasksRunner()
 
