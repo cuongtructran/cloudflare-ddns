b887b7a3088fdadfd9d8fd7b5d63994f7842c494
Tran Cuong Truc <cuong.t@scopicsoftware.com>
2020-01-28 12:45:05 +0700

    Add Vyper config

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-import logging.config
+import logging
+from logging.config import dictConfig
 
 import yaml
+from vyper import v
 
+from providers.configuration_provider import get_domains
 from schedulers.domain_update_scheduler import DomainUpdateScheduler
 from schedulers.schedule_task_runner import ScheduledTasksRunner
+
+
+def load_configs(configuration_files):
+    v.automatic_env()
+    v.set_env_key_replacer(".", "_")
+    v.set_config_type("yaml")
+    v.add_config_path("./resources/configurations")
+
+    for configuration_file in configuration_files:
+        v.set_config_name(configuration_file)
+        v.merge_in_config()
 
 
 def config_logger():
     with open('resources/log.yml', 'r') as f:
         config = yaml.safe_load(f.read())
-        logging.config.dictConfig(config)
+        dictConfig(config)
 
 
 def main():
     config_logger()
+    load_configs([
+        "cred",
+        "general"
+    ])
+
     logging.info("Started cloudflare-ddns application...")
     domain_update_scheduled_task = DomainUpdateScheduler()
     scheduler_runner = ScheduledTasksRunner()
--- providers/configuration_provider.py
+++ providers/configuration_provider.py
@@ -1,12 +1,11 @@
 import yaml
+from vyper import v
 
 
 def get_domains():
-    with open('resources/configurations/general.yml') as f:
-        domains_string = yaml.load(f, Loader=yaml.FullLoader).get('domains')
-        return domains_string.split(',')
+    domains_string = v.get("domains")
+    return domains_string.split(',')
 
 
 def get_interval():
-    with open('resources/configurations/general.yml') as f:
-        return yaml.load(f, Loader=yaml.FullLoader).get('check_interval')
+    return v.get("check_interval")
